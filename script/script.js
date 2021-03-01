67151b0abfc6e5f4b87d79dbd6376013bdd7e9a7
Alina <alinakazakbaeva@yandex.ru>
2021-03-01 12:20:38 +0300

    correction

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 document.querySelector('.menu-btn').onclick = function () {
   document.querySelector('.menu-btn').classList.toggle('active');
   document.querySelector('.dialog').classList.toggle('active');
+  document.querySelector('body').classList.toggle('active');
 };
