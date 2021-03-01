2e3d9fdeaf5a2c513984b729e540403f5780f7fa
Alina <alinakazakbaeva@yandex.ru>
2021-03-01 12:17:26 +0300

    menu-btn

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-btn').onclick = function () {
+  document.querySelector('.menu-btn').classList.toggle('active');
+  document.querySelector('.dialog').classList.toggle('active');
+};
